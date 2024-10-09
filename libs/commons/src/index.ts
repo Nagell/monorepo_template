d3d2c6895244c56496d0a79ae3be41939dfa82bd
Dawid Nitka <dawidnitka@gmail.com>
2024-10-09 18:55:29 +0200

    feat(@monorepo/commons): added basic composables and helper functions
    - Add useHttpClient composable function for making HTTP requests with axios
    - Add useLogger composable function for logging messages depending on the running mode
    - Add environment helper for checking the development mode
    - Add resetPiniaStore helper

---
+++ libs/commons/src/index.ts
@@ -0,0 +1,12 @@
+// Helpers
+import environment from './helpers/environment'
+import resetPiniaStore from './helpers/resetPiniaStore'
+
+export const helpers = {
+    environment,
+    resetPiniaStore,
+}
+
+// Composables
+export { useLogger } from './composables/useLogger'
+export { useHttpClient } from './composables/useHttpClient'
